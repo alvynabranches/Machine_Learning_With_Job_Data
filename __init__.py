a3892cc13fab2bfdb57d777988988e6bd2e270ca
Alvyn Abranches <alvynabranches@gmail.com>
2020-03-28 01:54:54 +0530

    28032020v2

--- __init__.py
+++ __init__.py
@@ -5,3 +5,9 @@ chrome_driver_location = 'D:/A/3T_Project/chromedriver.exe'
 PreProcessing_Done = False
 Machine_Learning_Done = False
 GUI_Done = False
+
+Libraries_Required = [
+    'threaded',
+    'selenium',
+    'multiprocessing'
+]
