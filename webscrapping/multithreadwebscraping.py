e71402ed83e07a92326933d5c42cd2b233ab97cf
alvynabranches <alvynabranches@gmail.com>
2020-04-22 22:55:07 +0530

    22042020v10

--- webscrapping/multithreadwebscraping.py
+++ webscrapping/multithreadwebscraping.py
@@ -20,7 +20,6 @@ def multiple_threaded_function(start, end, no_of_threads, location_list, job_lis
     '''
     load_on_single_thread = abs((end - start) // no_of_threads)
     _l = 0
-    _t = 0
     for location in location_list:
         _l += 1
         print(f'{_l} / {len(location_list)} Locations Processing')
@@ -28,9 +27,8 @@ def multiple_threaded_function(start, end, no_of_threads, location_list, job_lis
         location = str(location)
         for job in job_list:
             _j += 1
-            _t += 1
-            print(f'{_j} / {len(location_list)} Positions Processing')
-            print(f'{_t} / {len(location_list) * len(job_list)} Processing')
+            print(f'{_j} / {len(job_list)} Positions Processing')
+            print(f'{_l + _j} / {len(location_list) * len(job_list)} Processing')
             ts = []
             for i in range(0, no_of_threads):
                 ts.append(threaded_function((i+start+1) * load_on_single_thread, (i+start+1+1) * load_on_single_thread, location, job))
