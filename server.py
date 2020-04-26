8120857b60f25d9a845618d1c67b48dc5115fff5
alvynabranches <alvynabranches@gmail.com>
2020-04-27 01:32:43 +0530

    27042020v3

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from datetime import datetime, date, timedelta
 from time import perf_counter
 import pandas as pd
 import os
+from __init__ import project_directory
 # from pyspark import SparkContext, SparkConf
 
 # sc = SparkContext(conf=SparkConf().set("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.11:2.3.2"))
@@ -16,18 +17,13 @@ start = perf_counter()
 
 # multiple_threaded_function(start=0, end=3, no_of_threads=3, location_list=unique_locations, job_list=unique_jobs)
 
-merge_excel_files()
+# merge_excel_files()
 
 df = pd.read_excel('./indeed_results.xlsx')
 
 df = apply_preprocessing_on_fields(df)
 
 df.to_excel(f'indeed_results_pp_{str(date.today())}.xlsx', index=False)
-
-if os.path.isfile(f'./indeed_result{str(date.today() - timedelta(days=1))}'):
-    os.remove(f'./indeed_result{str(date.today() - timedelta(days=1))}')
-elif os.path.isfile(f'./indeed_result{str(date.today() - timedelta(days=2))}'):
-    os.remove(f'./indeed_result{str(date.today() - timedelta(days=2))}')
 
 finish = perf_counter()
 
